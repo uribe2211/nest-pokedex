aaf540d7e33a6e10d5f576f269b74555f993506e
Felipe Rodriguez <uribe2211@hotmail.com>
2023-12-27 22:16:58 -0600

    Antes de insertar por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -15,7 +16,9 @@ import { CommonModule } from './common/common.module';
 
     PokemonModule,
 
-    CommonModule
+    CommonModule,
+
+    SeedModule
   ],
 })
 export class AppModule {}
